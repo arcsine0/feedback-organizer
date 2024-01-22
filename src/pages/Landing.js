67b514922d4f7a7d82b196585a232e1729ee5167
Raphael Jans S. Caballegan <raphael.caballegan@gmail.com>
2024-01-22 12:03:12 +0800

    added tailwind

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,8 +1,9 @@
 export default function Landing() {
     return (
         <>
-            <h1>Welcome to LMS.ai</h1>
+            <h1 className="text-3xl font-bold">Welcome to LMS.ai</h1>
             <p>This will serve as the base project file for my LMS integrated with Vicuna 13-B</p>
+            <p>This project is also built with Tailwind.css</p>
         </>
     )
 }
