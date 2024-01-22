3fae2c1ec0499cbccf70c8bc22c59c013fac67f2
Raphael Jans S. Caballegan <raphael.caballegan@gmail.com>
2024-01-22 12:30:12 +0800

    Update Landing.js

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -2,7 +2,7 @@ export default function Landing() {
     return (
         <div className="flex flex-col p-10">
             <h1 className="text-3xl font-bold">Welcome to LMS.ai</h1>
-            <p>This will serve as the base project file for my LMS integrated with Vicuna 13-B</p>
+            <p>This will serve as the base project file for my LMS integrated with Vicuna 13-B LLM</p>
             <p>This project is also built with Tailwind.css</p>
         </div>
     )
