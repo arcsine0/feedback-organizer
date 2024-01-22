c5426cbf526e08fcf7a3a599f7812b71d7af3eaa
Raphael Jans S. Caballegan <raphael.caballegan@gmail.com>
2024-01-22 11:54:48 +0800

    cleaned

---
+++ src/pages/Landing.js
@@ -0,0 +1,8 @@
+export default function Landing() {
+    return (
+        <>
+            <h1>Welcome to LMS.ai</h1>
+            <p>This will serve as the base project file for my LMS integrated with Vicuna 13-B</p>
+        </>
+    )
+}
