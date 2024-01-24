ce8f5bb780b189d57edc35020ae2356661f488af
Raphael Jans S. Caballegan <raphael.caballegan@gmail.com>
2024-01-24 19:45:27 +0800

    working sidebar

--- src/index.js
+++ src/index.js
@@ -1,19 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 import './index.css';
 
 import Landing from './pages/Landing'
 import Sidebar from './components/Sidebar';
+import Courses from './pages/Courses';
+import Inbox from './pages/Inbox';
+import SignIn from './pages/SignIn';
 
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <div className='flex'>
-      <Sidebar />
-      <Landing title="LMS"/>
-    </div>
+    <BrowserRouter>
+        <div className='flex'>
+          <Sidebar />
+          <Routes>
+            <Route path='/' index element={<Landing title={'LMS'} />} />
+            <Route path='/courses' element={<Courses />} />
+            <Route path='/inbox' element={<Inbox />} />
+            <Route path='/signin' element={<SignIn />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
   </React.StrictMode>
 );
 
---
+++ src/pages/Inbox.js
@@ -0,0 +1,7 @@
+export default function Inbox() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
