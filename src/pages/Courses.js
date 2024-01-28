928d53325868d79b76a0cff3bad0cd16b0448d96
Raphael Jans S. Caballegan <raphael.caballegan@gmail.com>
2024-01-28 16:05:14 +0800

    added React-Icons

--- src/pages/Courses.js
+++ src/pages/Courses.js
@@ -1,7 +1,9 @@
 export default function Courses() {
     return (
-        <div>
-            
+        <div className="flex flex-col p-10">
+            <h1 className="text-3xl font-bold">Recent Courses</h1>
+            <br />
+            <h1 className="text-3xl font-bold">All Courses</h1>
         </div>
     )
 }
--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -6,6 +6,8 @@ export default function Landing({title}) {
             <p>This project is also built with Tailwind.css</p>
             <br />
             <p>et, consectetur adipiscing elit. Maecenas tortor odio, varius ac velit ac, consequat pharetra felis. Vestibulum varius ante orci, sed accumsan nunc vestibulum sed. Quisque vitae odio vitae lorem ultrices blandit sit amet in odio. Quisque tincidunt lacinia lacus vel iaculis. Duis gravida quis est et egestas. Curabitur sollicitudin, massa vitae sodales tincidunt, diam erat fringilla diam, nec euismod velit ligula sit amet metus. Maecenas felis neque, sodales nec nisi in, pharetra consequat eros. Proin semper tortor eget imperdiet pellentesque. Nulla eu tempor elit. Nunc gravida mi ac tortor luctus rhoncus. Aenean ut sem a mi ultrices sagittis. Vestibulum at cursus mi. Duis nec ex a arcu vehicula semper. Nullam ultricies eros at tristique euismod. Morbi rhoncus lobortis ipsum.</p>
+            <br />
+            <h1 className="text-3xl font-bold">Usage</h1>
         </div>
     )
 }
