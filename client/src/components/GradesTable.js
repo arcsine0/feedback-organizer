e1e6bad9225d0f630045e749176f8c7156915cad
arcsine0 <raphael.caballegan@gmail.com>
2024-02-06 19:15:51 +0800

    model testing

--- client/src/components/GradesTable.js
+++ client/src/components/GradesTable.js
@@ -1,15 +1,15 @@
 import { useState } from "react"
 
 export default function GradesTable() {
+    const [quiz, setQuiz] = useState(0);
+    const [lab, setLab] = useState(0);
+    const [exam, setExam] = useState(0);
+
     const [finalGrade, setFinalGrade] = useState(0);
     const [scaledGrade, setScaledGrade] = useState(0);
 
     const setScores = () => {
-        const quizVal = parseInt(document.getElementById('quizzes').value, 10) || 0;
-        const labVal = parseInt(document.getElementById('lab').value, 10) || 0;
-        const examVal = parseInt(document.getElementById('exam').value, 10) || 0;
-
-        const calcGrade = (quizVal * 0.3) + (labVal * 0.3) + (examVal * 0.4);
+        const calcGrade = (quiz * 0.3) + (lab * 0.3) + (exam * 0.4);
 
         setFinalGrade(calcGrade)
 
@@ -50,27 +50,33 @@ export default function GradesTable() {
                     <label htmlFor="quizzes" className="block text-sm font-medium leading-6 text-gray-900">Quizzes</label>
                     <input
                         type="number"
+                        value={quiz}
                         name="quizzes"
                         id="quizzes"
                         className="p-2 border-2 rounded-lg"
+                        onChange={(e) => setQuiz(e.target.value)}
                     />
                 </div>
                 <div>
                     <label htmlFor="lab" className="block text-sm font-medium leading-6 text-gray-900">Lab Activities</label>
                     <input
                         type="number"
+                        value={lab}
                         name="lab"
                         id="lab"
                         className="p-2 border-2 rounded-lg"
+                        onChange={(e) => setLab(e.target.value)}
                     />
                 </div>
                 <div>
                     <label htmlFor="exam" className="block text-sm font-medium leading-6 text-gray-900">Final Exam</label>
                     <input
                         type="number"
+                        value={exam}
                         name="exam"
                         id="exam"
                         className="p-2 border-2 rounded-lg"
+                        onChange={(e) => setExam(e.target.value)}
                     />
                 </div>
                 <button onClick={setScores} className="flex h-5 p-5 justify-center items-center shadow-md rounded-md text-white font-semibold bg-gradient-to-r from-sky-500 to-indigo-500">Submit</button>
