373209316d4795379917d5286ba065c94830b3d1
Raphael Jans S. Caballegan <raphael.caballegan@gmail.com>
2024-02-21 22:47:28 +0800

    renamed Sidebar list item as well

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -34,14 +34,14 @@ export default function Sidebar({uname}) {
                             <Menu as="div" className="flex flex-col space-y-2 w-full h-full">
                                 <Menu.Button className='flex p-2 items-center dark:text-white text-gray-900 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700 group'>
                                     <FaThLarge className='order-first' />
-                                    <span className='ms-3'>Sources</span>
+                                    <span className='ms-3'>Instances</span>
                                     <div className='grow'></div>
                                     <FaChevronDown className='order-last' />
                                 </Menu.Button>
                                 <Menu.Items className="flex flex-col">
                                     <Menu.Item>
                                         <a className='p-1 dark:text-white text-gray-900 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700 group'>
-                                            <Link to={'/instances'}><span className='ms-3'>All Sources</span></Link>
+                                            <Link to={'/instances'}><span className='ms-3'>All Instances</span></Link>
                                         </a>
                                     </Menu.Item>
                                     {/* <Menu.Item>
