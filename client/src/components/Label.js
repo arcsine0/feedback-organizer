863d30a6f5dfc82e8065c62197bcc8c380d91b1d
Raphael Jans S. Caballegan <raphael.caballegan@gmail.com>
2024-02-22 23:33:24 +0800

    added isBold attr to Labels

--- client/src/components/Label.js
+++ client/src/components/Label.js
@@ -1,7 +1,9 @@
-export default function Label({ name, remove }) {
+export default function Label({ name, remove, isBold }) {
     return (
         <div onClick={() => remove(name)} className="flex p-2 justify-center items-center bg-slate-200 rounded-lg shadow-md cursor-pointer">
-            <h1 className="text-sm font-bold text-black select-none">{name}</h1>
+            <span className={isBold ? "font-bold" : ""}>
+                <h1 className="text-sm text-black select-none">{name}</h1>
+            </span>
         </div>
     )
 }
--- client/src/pages/Sources.js
+++ client/src/pages/Sources.js
@@ -7,10 +7,17 @@ import { db } from "../firebase/config";
 
 import { FaPencilAlt, FaChevronDown } from "react-icons/fa";
 
+import Label from "../components/Label";
+
 export default function Sources() {
     const [instances, setInstances] = useState([]);
     const [selectedInstance, setSelectedInstance] = useState("Select instance");
     const [selectedInstanceName, setSelectedInstanceName] = useState("None");
+
+    const [feedback, setFeedback] = useState("");
+    const [feedbacks, setFeedbacks] = useState([]);
+
+    const [feedbackError, setFeedbackError] = useState("");
 
     useEffect(() => {
         getDocs(collection(db, "ClientInstances"))
@@ -20,7 +27,7 @@ export default function Sources() {
                         id: doc.id,
                         title: doc.data().title
                     }
-                    
+
                     if (!instances.find(ins => ins.id === result.id)) {
                         let newInstance = [...instances, result];
                         setInstances(newInstance);
@@ -40,11 +47,47 @@ export default function Sources() {
         setSelectedInstanceName(instanceName);
     }
 
+    const preProcessFeedback = (fd) => {
+        if (fd.startsWith('"')) {
+            fd = fd.slice(1);
+        }
+
+        if (fd.endsWith('"')) {
+            fd = fd.slice(0, -1);
+        }
+
+        let processedString = fd.replace(/"/g, "'");
+
+        return processedString;
+    }
+
+    const addFeedback = () => {
+        let processedFeedback = preProcessFeedback(feedback);
+
+        if (!feedbacks.includes(processedFeedback) && processedFeedback !== "") {
+            let newFeedbacks = [...feedbacks, processedFeedback]
+            setFeedbacks(newFeedbacks);
+            setFeedback("");
+        }
+    }
+
+    const removeFeedback = (name) => {
+        if (feedbacks.length > 1) {
+            let newFeedbacks = feedbacks.filter(fd => fd !== name)
+            setFeedbacks(newFeedbacks);
+
+
+            setFeedbackError("");
+        } else {
+            setFeedbackError("There should at least be 1 tag");
+        }
+    }
+
     return (
         <div className="flex flex-col w-full h-full p-10 space-y-10">
             <div className="flex flex-col space-y-5">
                 <h1 className="text-3xl font-bold">Feedback Inputs</h1>
-                <div className="p-5 w-1/3 flex flex-col space-y-2 justify-center border-2 border-dashed border-black">
+                <div className="p-5 w-1/3 flex flex-col space-y-2 justify-center border-2 border-dashed border-black rounded-md">
                     <p>Selected Instance:</p>
                     <h1 className="text-2xl font-bold">{selectedInstanceName}</h1>
                     <Listbox as="div" className="p-2 bg-slate-200 rounded-lg"
@@ -57,7 +100,7 @@ export default function Sources() {
                             <span className="order-last"><FaChevronDown /></span>
                         </Listbox.Button>
                         <Listbox.Options className="space-y-1">
-                            {instances.map((ins, ind) => (
+                            {instances.map((ins) => (
                                 <Listbox.Option
                                     key={ins.id}
                                     value={ins.id}
@@ -68,6 +111,43 @@ export default function Sources() {
                             ))}
                         </Listbox.Options>
                     </Listbox>
+                </div>
+                <div className="flex flex-col space-y-2">
+                    <h1 className="text-3xl font-bold">Source Type</h1>
+                    <Tab.Group>
+                        <Tab.List className="flex w-1/3 space-x-10 p-2 items-center">
+                            <Tab className="flex justify-center items-center px-5 py-2 hover:border-b-2 border-black">
+                                <h1 className="font-semibold">Simple</h1>
+                            </Tab>
+                            <h1>|</h1>
+                            <Tab className="flex justify-center items-center px-5 py-2 hover:border-b-2 border-black">
+                                <h1 className="font-semibold">Google Sheets</h1>
+                            </Tab>
+                        </Tab.List>
+                        <Tab.Panels className="mt-6">
+                            <Tab.Panel as={"div"} className="flex flex-col w-1/3 space-y-4">
+                                <h1 className="text-2xl font-bold">Send Feedbacks</h1>
+                                <div className="flex flex-col space-y-1">
+                                    <p>Enter or Paste feedbacks below:</p>
+                                    <div className="flex flex-row space-x-4 items-center">
+                                        <input
+                                            type="textarea"
+                                            name="feedbacks"
+                                            className="p-2 grow h-full border-2 border-black rounded-lg text-pretty"
+                                            value={feedback}
+                                            onChange={(e) => setFeedback(e.target.value)}
+                                        />
+                                        <button onClick={addFeedback} className="flex h-5 shrink p-5 justify-center items-center shadow-md rounded-md text-white font-semibold bg-gradient-to-r from-sky-500 to-indigo-500">Add</button>
+                                    </div>
+                                </div>
+                                <div className="flex flex-row flex-wrap p-2 space-x-2 border-2 border-dashed border-black rounded-md">
+                                    {feedbacks.map((fd) => (
+                                        <Label name={fd} remove={removeFeedback} isBold={false} />
+                                    ))}
+                                </div>
+                            </Tab.Panel>
+                        </Tab.Panels>
+                    </Tab.Group>
                 </div>
             </div>
         </div>
