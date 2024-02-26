081ee7e51e6e8fa5a5cc50c8e633a89bc222ce1b
Raphael Jans S. Caballegan <raphael.caballegan@gmail.com>
2024-02-26 12:56:54 +0800

    added ENV config

--- client/src/firebase/config.js
+++ client/src/firebase/config.js
@@ -7,14 +7,16 @@ import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAyU2PpTlsLWBdcWQcEIjoUj3zLeWOJxMU",
-  authDomain: "feedback-org-f46b9.firebaseapp.com",
-  projectId: "feedback-org-f46b9",
-  storageBucket: "feedback-org-f46b9.appspot.com",
-  messagingSenderId: "759888812002",
-  appId: "1:759888812002:web:a91b555f37320f254f4bbb",
-  measurementId: "G-9G1F3QG65D"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 // Initialize Firebase
