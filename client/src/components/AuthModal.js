859d295a6f267b19a3d65a3b98f00166fc11e01a
Raphael Jans S. Caballegan <raphael.caballegan@gmail.com>
2024-03-05 19:32:33 +0800

    removed unnecessary components

--- client/src/components/AuthModal.js
+++
@@ -1,9 +0,0 @@
-export default function AuthModal({ isShown }) {
-    return (
-        <div className="flex fixed h-full w-full z-10 justify-center items-center bg-black bg-opacity-50" onClick={() => isShown(false)}>
-            <div className="flex w-1/3 h-1/5 justify-center items-center bg-white rounded-lg shadow-md">
-                <h1 className="font-bold text-4xl">Login Success</h1>
-            </div>
-        </div>
-    )
-}
