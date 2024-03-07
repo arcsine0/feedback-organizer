3896f1f35902ad1c261e6db9b7e23893da20b67f
Raphael Jans S. Caballegan <raphael.caballegan@gmail.com>
2024-03-07 22:11:31 +0800

    added google auth for Logins

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -3,8 +3,8 @@ import { useNavigate, Link } from "react-router-dom";
 
 import isEmail from "validator/lib/isEmail";
 
-import { doc, getDoc } from "firebase/firestore";
-import { signInWithEmailAndPassword } from "firebase/auth";
+import { doc, getDoc, setDoc } from "firebase/firestore";
+import { signInWithEmailAndPassword, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import { db, auth } from "../firebase/config";
 
 import GlobalContext from "../globals/GlobalContext";
@@ -13,11 +13,17 @@ export default function Login() {
     const [email, setEmail] = useState("");
     const [pass, setPass] = useState("");
 
+    const [loginLabel, setLoginLabel] = useState("Log In");
+    const [loginDisabled, setLoginDisabled] = useState(false);
+
     const { globalState, setGlobalState } = useContext(GlobalContext);
 
     const navigate = useNavigate();
 
     const submitCredentials = async () => {
+        setLoginLabel("Logging In...");
+        setLoginDisabled(true);
+
         if (isEmail(email)) {
             if (pass.length > 0) {
                 try {
@@ -39,6 +45,9 @@ export default function Login() {
 
                                     localStorage.setItem("isLoggedIn", true);
 
+                                    setLoginLabel("Logged In");
+                                    setLoginDisabled(false);
+
                                     navigate("/");
                                 });
                         });
@@ -49,6 +58,59 @@ export default function Login() {
                 }
             } else { console.log("Password Empty"); }
         } else { console.log("Email Invalid"); }
+
+        setLoginLabel("Log In");
+        setLoginDisabled(false);
+    }
+
+    const googleLogin = async () => {
+        try {
+            const provider = new GoogleAuthProvider();
+            await signInWithPopup(auth, provider)
+                .then(async (res) => {
+                    const user = res.user;
+
+                    localStorage.setItem("id", user.uid);
+                    try {
+                        await getDoc(doc(db, "ClientAccounts", user.uid))
+                            .then(async (sn) => {
+                                if (sn.data()) {
+                                    localStorage.setItem("uname", sn.data().uname);
+
+                                    setGlobalState({
+                                        ...globalState,
+                                        isLoggedIn: true,
+                                        id: user.uid,
+                                        uname: sn.data().uname
+                                    });
+                                } else {
+                                    await setDoc(doc(db, "ClientAccounts", user.uid), {
+                                        uname: user.displayName,
+                                        firstName: "",
+                                        lastName: ""
+                                    }).then(() => {
+                                        setGlobalState({
+                                            ...globalState,
+                                            isLoggedIn: true,
+                                            id: user.uid,
+                                            uname: user.displayName
+                                        });
+                                    });
+                                }
+
+                                localStorage.setItem("isLoggedIn", true);
+
+                                navigate("/");
+                            });
+                    } catch (error) {
+                        console.log(error);
+                    }
+
+
+                });
+        } catch (error) {
+            console.log(error);
+        }
     }
 
     return (
@@ -77,7 +139,8 @@ export default function Login() {
                         onChange={e => setPass(e.target.value)}
                     />
                 </div>
-                <button onClick={submitCredentials} className="flex h-5 w-3/4 p-5 justify-center items-center shadow-md rounded-md text-white font-semibold bg-gradient-to-r from-sky-500 to-indigo-500">Log In</button>
+                <button onClick={submitCredentials} disabled={loginDisabled} className="flex h-5 w-3/4 p-5 justify-center items-center shadow-md rounded-md text-white font-semibold bg-gradient-to-r from-sky-500 to-indigo-500">{loginLabel}</button>
+                <button onClick={googleLogin} className="flex h-5 w-3/4 p-5 justify-center items-center shadow-md rounded-md text-black font-semibold bg-gradient-to-r from-white to-slate-300">Log In with Google</button>
                 <p>No Account Yet?</p>
                 <Link to={"/register"}>
                     <p className="text-blue-500 select-none cursor-pointer">Create an Account</p>
