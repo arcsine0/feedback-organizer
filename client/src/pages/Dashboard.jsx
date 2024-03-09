6fe4ef054394b42c41471f54bbdad9929e9fed70
Raphael Jans S. Caballegan <raphael.caballegan@gmail.com>
2024-03-09 13:38:41 +0800

    removed Usage in Dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -7,9 +7,9 @@ export default function Dashboard() {
                 <p>This project is also built with Tailwind.css</p>
             </div>
             <p>et, consectetur adipiscing elit. Maecenas tortor odio, varius ac velit ac, consequat pharetra felis. Vestibulum varius ante orci, sed accumsan nunc vestibulum sed. Quisque vitae odio vitae lorem ultrices blandit sit amet in odio. Quisque tincidunt lacinia lacus vel iaculis. Duis gravida quis est et egestas. Curabitur sollicitudin, massa vitae sodales tincidunt, diam erat fringilla diam, nec euismod velit ligula sit amet metus. Maecenas felis neque, sodales nec nisi in, pharetra consequat eros. Proin semper tortor eget imperdiet pellentesque. Nulla eu tempor elit. Nunc gravida mi ac tortor luctus rhoncus. Aenean ut sem a mi ultrices sagittis. Vestibulum at cursus mi. Duis nec ex a arcu vehicula semper. Nullam ultricies eros at tristique euismod. Morbi rhoncus lobortis ipsum.</p>
-            <div className="flex flex-col space-y-1">
+            {/* <div className="flex flex-col space-y-1">
                 <h1 className="text-3xl font-bold">Usage</h1>
-            </div>
+            </div> */}
             {/* <div className="flex flex-col space-y-1">
                 <h1 className="text-3xl font-bold">Grade Calculator</h1>
                 <GradesTable />
